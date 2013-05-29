540ac9bf36b99d2b0e4f5da90d6261a830b01470
Göran Wahlsten <goran.wahlsten@acando.com>
2013-05-29 11:15:58 +0200

    Improved
    Signed-off-by: Göran Wahlsten <goran.wahlsten@acando.com>

--- src/Hello.java
+++ src/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello World!!!");
+		System.out.println("Hello improved World!!!");
 	}
 
 }
