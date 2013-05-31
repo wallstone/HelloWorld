7c3283eadc5455ab9c10211f757e82a90241b9ee
Göran Wahlsten <goran.wahlsten@acando.com>
2013-05-31 11:18:38 +0200

    bjbjj

--- src/Hello.java
+++ src/Hello.java
@@ -5,7 +5,8 @@ public class Hello {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello improved even more World!!!");
+		System.out.println("Hello improved even more World!");
+		System.out.println("Hello World!");
 	}
 
 }
